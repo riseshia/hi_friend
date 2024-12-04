5f669d9adbd043fe39544abd35ef2da544f40756
Shia <rise.shia@gmail.com>
2024-12-04 17:25:16 +0900

    Add index by path in tvar registry

--- lib/hi_friend/core/type_variable_registry.rb
+++ lib/hi_friend/core/type_variable_registry.rb
@@ -1,35 +1,44 @@
 module HiFriend::Core
   class TypeVariableRegistry
     def initialize
-      @registry = {}
+      @tv_by_id = {}
+      @tvs_by_path = Hash.new { |h, k| h[k] = [] }
     end
 
     def add(var)
-      @registry[var.id] = var
-    end
-
-    def remove(node_id)
-      @registry.delete(node_id)
+      @tv_by_id[var.id] = var
+      @tvs_by_path[var.path] << var
     end
 
     def remove_by_path(path)
-      @registry.delete_if { |_, var| var.path == path }
+      tvs = @tvs_by_path.delete(path)
+
+      if tvs
+        tvs.each do |tv|
+          @tv_by_id.delete(tv.id)
+        end
+      end
     end
 
     def find(node_id)
-      @registry[node_id]
+      @tv_by_id[node_id]
     end
 
+    def find_by_path(path)
+      @tvs_by_path[path]
+    end
+
+    # test purpose
     def all_keys
-      @registry.keys
+      @tv_by_id.keys
     end
 
     def all
-      @registry.values
+      @tv_by_id.values
     end
 
     def clear
-      @registry.clear
+      @tv_by_id.clear
     end
   end
 end
