7257c1e42e28e1a2459335222558f007ce41b149
Shia <rise.shia@gmail.com>
2024-12-28 20:07:01 +0900

    Fix test

--- spec/hi_friend/core/visitor_const_parse_spec.rb
+++ spec/hi_friend/core/visitor_const_parse_spec.rb
@@ -48,7 +48,7 @@ module HiFriend::Core
       it "registers all" do
         ref_const = type_var_registry.all.first
 
-        expect(ref_const.infer.to_human_s).to eq("C")
+        expect(ref_const.infer.to_human_s).to eq("singleton(C)")
       end
     end
 
@@ -68,7 +68,7 @@ module HiFriend::Core
       it "registers all" do
         ref_const = type_var_registry.all.first
 
-        expect(ref_const.infer.to_human_s).to eq("A::B")
+        expect(ref_const.infer.to_human_s).to eq("singleton(A::B)")
       end
     end
 
@@ -93,10 +93,10 @@ module HiFriend::Core
       it "registers all" do
         abs_const, rel_const0, rel_const1, rel_const2 = type_var_registry.all
 
-        expect(abs_const.infer.to_human_s).to eq("A")
-        expect(rel_const0.infer.to_human_s).to eq("A::B::A")
-        expect(rel_const1.infer.to_human_s).to eq("A::B::A::B")
-        expect(rel_const2.infer.to_human_s).to eq("A::B")
+        expect(abs_const.infer.to_human_s).to eq("singleton(A)")
+        expect(rel_const0.infer.to_human_s).to eq("singleton(A::B::A)")
+        expect(rel_const1.infer.to_human_s).to eq("singleton(A::B::A::B)")
+        expect(rel_const2.infer.to_human_s).to eq("singleton(A::B)")
       end
     end
 
@@ -114,7 +114,7 @@ module HiFriend::Core
       it "registers all" do
         one = type_var_registry.all.first
 
-        expect(one.infer.to_human_s).to eq("C")
+        expect(one.infer.to_human_s).to eq("singleton(C)")
       end
     end
 
